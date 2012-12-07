cf48d118ff8a01d520ba81de8f42368428787024
Tony Nelson <tnelson@starpoint.com>
2012-12-07 11:16:47 -0500

    Added more columns to the sortdemo to make it more obvious what is going on

--- src/main/java/com/starpoint/datasource/SortDemoDataSource.java
+++ src/main/java/com/starpoint/datasource/SortDemoDataSource.java
@@ -4,13 +4,11 @@ import com.starpoint.domain.User;
 import org.apache.tapestry5.grid.ColumnSort;
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.grid.SortConstraint;
+import org.mvel2.MVEL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  */
@@ -19,10 +17,14 @@ public class SortDemoDataSource implements GridDataSource {
     private List<User> users;
 
     public SortDemoDataSource(int maxUsers) {
+        Random random = new Random(21020824L);  // not so random on purpose
+
         users = new ArrayList<User>();
 
         for (int i = 0; i < maxUsers; i++) {
-            users.add(new User("user" + String.format("%04d", i)));
+            String id = String.format("%04d", i);
+            String randomId = String.format("%04d", random.nextInt(maxUsers));
+            users.add(new User("user" + id, "first" + id, "last" + randomId));
         }
     }
 
@@ -34,7 +36,9 @@ public class SortDemoDataSource implements GridDataSource {
     @Override
     public void prepare(int startIndex, int endIndex, List<SortConstraint> sortConstraints) {
 
-        Collections.sort(users, new SortDemoSorter(sortConstraints.get(0).getColumnSort()));
+        SortConstraint sortConstraint = sortConstraints.get(0);
+        Collections.sort(users, new SortDemoSorter(sortConstraint.getPropertyModel().getPropertyName(),
+                sortConstraint.getColumnSort()));
     }
 
     @Override
@@ -49,20 +53,23 @@ public class SortDemoDataSource implements GridDataSource {
 
     static class SortDemoSorter implements Comparator<User> {
 
+        private String property;
         private ColumnSort columnSort;
         private Logger logger = LoggerFactory.getLogger(SortDemoSorter.class);
 
-        SortDemoSorter(ColumnSort columnSort) {
+        SortDemoSorter(String property, ColumnSort columnSort) {
+            this.property = property;
             this.columnSort = columnSort;
-            logger.info("Created SortDemoSort with columnSort " + columnSort.name());
+            logger.info("Created SortDemoSort with property: " + property + ", columnSort: " + columnSort.name());
         }
 
         @Override
         public int compare(User user1, User user2) {
 
-            return columnSort == ColumnSort.ASCENDING ?
-                    user1.getUsername().compareTo(user2.getUsername()) :
-                    user2.getUsername().compareTo(user1.getUsername());
+            String v1 = (String)MVEL.getProperty(property, user1);
+            String v2 = (String)MVEL.getProperty(property, user2);
+
+            return columnSort == ColumnSort.ASCENDING ? v1.compareTo(v2) : v2.compareTo(v1);
         }
     }
 }
--- src/main/java/com/starpoint/domain/User.java
+++ src/main/java/com/starpoint/domain/User.java
@@ -5,12 +5,22 @@ package com.starpoint.domain;
 public class User {
 
     private String username;
+    private String firstName;
+    private String lastName;
 
     public User() {
     }
 
     public User(String username) {
         this.username = username;
+        this.firstName = "firstName";
+        this.lastName = "lastName";
+    }
+
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getUsername() {
@@ -19,5 +29,21 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
